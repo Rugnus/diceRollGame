0adf5f96ef2617c95ce3d470c9dfd77ae0825ce6
Rugnus <sungur.gasanov@mail.ru>
2021-10-06 11:09:36 +0300

    First and final commit

--- diceRollGame/ViewController.swift
+++ diceRollGame/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let diceNumberOne = arc4random_uniform(6) + 1
+        let diceNumberTwo = arc4random_uniform(6) + 1
+        
+        label.text = "Сумма кубиков равна: \(diceNumberOne + diceNumberTwo)"
+        
+        leftImageView.image = UIImage(named: "Dice\(diceNumberOne)")
+        
+        rightImageView.image = UIImage(named: "Dice\(diceNumberTwo)")
+    }
+    
+    
 }
 
